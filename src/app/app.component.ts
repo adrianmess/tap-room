4d4040b5612eb29d377d0901407bb5f85aa92050
Adrian Messado <adrian.messado@gmail.com>
2018-05-02 10:42:34 -0700

    add property bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Recipe } from './models/recipe.model';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,8 +14,22 @@ export class AppComponent {
   sushiRecipe = `* 6 sheets sushi seaweed` + ' * 1 batch prepared sushi rice' + ' * 1/2 lb sushi-grade raw salmon, or desired raw fish of choice.' + ' * 1 avocado, sliced' + ' * soy sauce, for serving';
 
   recipe: Recipe[] = [
-   new Recipe('Sushi', this.sushiRecipe, this.sushiDirections),
-   new Recipe('Stuffed Peppers', ['peppers'], ['bake']),
-   new Recipe('Speghetti', ['noodles'], ['boil'])
- ];
+    new Recipe('Sushi', this.sushiRecipe, this.sushiDirections, 2),
+    new Recipe('Stuffed Peppers', ['peppers'], ['bake'], 3),
+    new Recipe('Speghetti', ['noodles'], ['boil'], 1)
+  ];
+  editRecipe(){
+    alert('Time to edit a Recipe!');
+  }
+
+  priorityColor(currentRecipe){
+      if (currentRecipe.priority === 3){
+        return "bg-danger";
+      } else if (currentRecipe.priority === 2) {
+        return  "bg-warning";
+      } else {
+        return "bg-success";
+      }
+    }
+
 }
